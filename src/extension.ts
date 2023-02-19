12b9f3d73ef2949ad5c52316751f7f80f5bc8d47
Ashish Dhamala <ashish.dhamala2015@gmail.com>
2023-02-19 15:34:34 +0000

    cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { generateViewFile } from "./generate-view-file";
 
@@ -11,18 +9,8 @@ export async function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "laravel-utils" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand('laravel-utils.createViewFile', () => {
-    // The code you place here will be executed every time your command is executed
     generateViewFile();
-
-    // const text2 = editor?.document.getText();
-    // editor?.document.lineAt(editor.document.lineCount)
-
-    // Display a message box to the user
-    // vscode.window.showInformationMessage(`This is the text.`);
   });
 
   context.subscriptions.push(disposable);
